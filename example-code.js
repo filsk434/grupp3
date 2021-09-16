57804a01c4dfb9f7439b24bf6f1e0259eee2fd3b
Simon Mekonen <mekonen.simon@gmail.com>
2021-09-16 10:30:23 +0200

    Slutredigerad. Behöver flytta in all CSS till en fil och gömma class 'non-mobile' vid webkit mobile

--- example-code.js
+++ example-code.js
@@ -2,26 +2,32 @@ window.addEventListener('load', () => {
 
   const button = document.querySelector('.button');
   const selector = document.querySelectorAll('.javascript')
-  const result = document.querySelector('.result');
+  const console = document.querySelector('.console'); 
 
   button.addEventListener("click", () =>{
+    button.disabled = true;
+
     const funcDelay = document.querySelectorAll('.func-delay');
     const funcCompress = document.querySelectorAll('.func-compress');
     const funcEncrypt = document.querySelectorAll('.func-encrypt');
-    const funcCompEnc = document.querySelectorAll('.func-compEnc');
-    const runScript = document.querySelectorAll('.run-script');
+    const funcRunProgram = document.querySelectorAll('.func-compEnc');
+    const runProgram = document.querySelectorAll('.run-script');
+
+
+    console.classList.add('console-show');
+    console.classList.remove('console-hide');
 
 
     const delay = (ms) => new Promise((resolve) => setTimeout(resolve, ms));
 
     /* ORDER OF OPERATION ON WEBPAGE
-    1 runScript
-    2 funcCompEnc
+    1 runProgram
+    2 funcRunProgram
     3 funcCompress
     4 funcDelay
     5 funcEncrypt
     6 funcDelay
-    7 runScript
+    7 runProgram
     */
     
     // set all to blurred
@@ -29,22 +35,36 @@ window.addEventListener('load', () => {
       element.classList.add('blurred');
     });
 
-    async function runProgram(){
-    await activeOrInactive(runScript);
-    await activeOrInactive(funcCompEnc, 3000);
-    await activeOrInactive(funcCompress);
+    const standardDelay = 0;
+
+    async function start(){
+    await activeOrInactive(runProgram, -standardDelay);
+    await activeOrInactive(funcRunProgram);
+    console.innerHTML += '<p>1a) runProgram</p>';
+
+    await activeOrInactive(funcCompress, 3000);
+    console.innerHTML += '<p>2) compress</p>';
+
     await activeOrInactive(funcDelay);
-    await activeOrInactive(funcCompEnc);
+    
+    await activeOrInactive(funcRunProgram);
+    console.innerHTML += '<p>1b) runProgram</p>';
+    
     await activeOrInactive(funcDelay);
+    
     await activeOrInactive(funcEncrypt, 2000);
+    console.innerHTML += '<p>3) encrypt</p>';
+
     await activeOrInactive(funcDelay);
-    await activeOrInactive(runScript);
+    
+    await activeOrInactive(runProgram);
+    console.innerHTML += '<p>1c) runProgram</p>';
     await delay(3000);
     return;
     }
     
     async function activeOrInactive(nodelist, ms=0){
-      await delay(ms + 4000);
+      await delay(ms + standardDelay);
       selector.forEach(element => {
         element.classList.add('blurred');
         element.classList.remove('sharp');
@@ -56,12 +76,13 @@ window.addEventListener('load', () => {
       return;
     }   
 
-    runProgram().then(() => {
+    start().then(() => {
       selector.forEach(element => {
         element.classList.remove('blurred')
         element.classList.remove('sharp')
       })
-      alert('console.log(result) => eht');
+      console.innerHTML += 'eht';
+      button.disabled = false;
     })
 
 
